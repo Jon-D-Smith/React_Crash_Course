457bed446581222475844a114b03fff316049a74
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-19 17:31:17 -0500

    Created components; added delete method to remove tasks from the ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import { useState} from 'react'
+
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
+
+
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appt',
+        day: 'Feb 5th at 2:30 PM',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Dentist Appt',
+        day: 'Feb 5th at 3:30 PM',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'nail salon Appt',
+        day: 'Feb 5th at 4:30 PM',
+        reminder: false
+    }
+])
+
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task)=> task.id !== id))
+}
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks'}
     </div>
   );
 }
