86a73fc58ff9415f2b178264c22943308faa5868
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-20 19:00:31 -0500

    Added AddTask component to render form for tasks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState} from 'react'
 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-
+import AddTask from './components/AddTask'
 
 
 function App() {
@@ -28,6 +28,14 @@ function App() {
     }
 ])
 
+//Add Task
+
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 1000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 
 // Delete Task
 const deleteTask = (id) => {
@@ -42,6 +50,7 @@ const toggleReminder = (id) => {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks'}
     </div>
   );
